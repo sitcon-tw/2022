d1cbb4114a8363cab335d5caa0983484e27cbe99
gnehs <jayuiop@gmail.com>
2022-04-24 01:19:36 +0800

    import fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,5 +2,13 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Barlow:wght@500&family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap', crossorigin: true }
+      ],
+    }
+  }
 })
