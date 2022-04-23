969d6a8cbfedb2ecb0c56fbe4134a3dfe95cd38d
gnehs <jayuiop@gmail.com>
2022-04-24 01:19:44 +0800

    created pages

---
+++ pages/cfp/index.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <Head>
+    <Title>{{ title }}</Title>
+    <Meta name="description" :content="title" />
+    <Style type="text/css" children="body { background-color: green; }" />
+  </Head>
+  aaaa
+</template>
+<script setup>
+const title = ref('SITCON 2022 - Call for Papers')
+</script>
