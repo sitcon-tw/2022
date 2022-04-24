219297e18ad0736ced728288b28aeee55dc34531
gnehs <jayuiop@gmail.com>
2022-04-24 21:53:20 +0800

    removed RoundBtn.vue target="_blank"

--- components/cfp/RoundBtn.vue
+++ components/cfp/RoundBtn.vue
@@ -1,5 +1,5 @@
 <template>
-  <a :href="href" target="_blank">
+  <a :href="href">
     <span>
       {{ content }}
     </span>
@@ -12,21 +12,22 @@ export default {
 }
 </script>
 <style lang="sass" scoped>
-  a
-    display: block
-    width: 100%
-    white-space: nowrap
-    text-align: center
-    font-size: 24px
-    color: #fff
-    background-color: #32E0C4
-    border: 1px solid #32E0C4
-    text-decoration: none
-    max-width: 175px
-    font-weight: 700
-    border-radius: 50px
-    span
-      letter-spacing: 0.1em
-    &:hover
-      background-color: transparent
+
+a
+  display: block
+  width: 100%
+  white-space: nowrap
+  text-align: center
+  font-size: 24px
+  color: #fff
+  background-color: #32E0C4
+  border: 1px solid #32E0C4
+  text-decoration: none
+  max-width: 175px
+  font-weight: 700
+  border-radius: 50px
+  span
+    letter-spacing: 0.1em
+  &:hover
+    background-color: transparent
 </style>
