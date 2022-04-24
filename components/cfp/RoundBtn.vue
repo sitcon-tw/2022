9cd8e5b7610c4050f3531925a421276c95974bd4
Leko <leko@leko.moe>
2022-04-24 18:52:15 +0800

    main page

---
+++ components/cfp/RoundBtn.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="round-btn">
+      <a :href="href" target="_blank">
+        <span>
+            {{ content }}
+        </span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['href', 'content']
+}
+</script>
+<style lang="sass" scoped>
+
+div
+  display: flex
+  justify-content: flex-end
+  a
+    display: block
+    width: 100%
+    text-align: center
+    font-size: 24px
+    color: #fff
+    background-color: #32E0C4
+    border: 1px solid #32E0C4
+    text-decoration: none
+    max-width: 175px
+    font-weight: 700
+    border-radius: 50px
+    span
+      letter-spacing: 0.1em
+    &:hover
+      background-color: transparent
+</style>
--- pages/cfp/index.vue
+++ pages/cfp/index.vue
@@ -6,13 +6,25 @@
       <Meta name="description" :content="title" />
     </Head>
     <div class="container">
-      <logo-sitcon />
+      <div class="logos">
+        <logo-sitcon />
+        <logo-cat-in-a-maze />
+      </div>
+      <div class="稿件徵求中">
+        <img src="/imgs/cfp/稿件徵求中.svg">
+      </div>
 
-      <logo-cat-in-a-maze />
-
-      <router-link to="/cfp/normal">一般議程</router-link>
+      <cfp-round-btn href="/cfp/normal" content="一般議程" />
       <br>
-      <router-link to="/cfp/freeform">開放式議程</router-link>
+      <cfp-round-btn href="/cfp/freeform" content="開放式議程" />
+      <br>
+
+      <cfp-glass-box first-child>
+        <h3>關於 SITCON</h3>
+        <p>SITCON 學生計算機年會自 2013 年發起，以學生為本、由學生自發舉辦，長期投身學生資訊教育與推廣開源精神，希望引領更多學子踏入資訊的殿堂，更冀望所有對資訊有興趣的學生，能夠在年會裏齊聚一堂，彼此激盪、傳承、啟發，達到「學以致用、教學相長」的實際展現。</p>
+        <p>在 SITCON 的第 X 週年，期望我們秉持初衷——讓學生們分享與交流知識，把各地學生串連起來的 X 字路口——也期望每人皆能在與大家的聊天與討論之中，找到自己的方向、熱情。</p>
+        <h3>SITCON 歷年網站</h3>
+      </cfp-glass-box>
 
     </div>
     <Footer />
@@ -32,6 +44,8 @@ const title = ref('SITCON 2022 - Call for Papers')
     padding: 40px 20px
     width: calc(100% - 40px)
     margin: 18px auto
+  @media screen and (max-width: 650px)
+    padding: 40px 5vw
   ::v-deep(.main-title)
     justify-content: flex-end
     gap: 96px
@@ -39,4 +53,23 @@ const title = ref('SITCON 2022 - Call for Papers')
     @media screen and (max-width: 768px)
       font-size: 20px
       gap: 32px
+  .logos
+    white-space: nowrap
+
+.稿件徵求中
+  display: flex
+  justify-content: flex-end
+  margin: 7vh 3vw
+  @media screen and (max-width: 1280px)
+    margin: 6vh 4vw
+  @media screen and (max-width: 768px)
+    margin: 3vh 3vw
+  img
+    width: 70%
+    @media screen and (max-width: 1280px)
+      width: 60%
+    @media screen and (max-width: 768px)
+      width: 45%
+    @media screen and (max-width: 650px)
+      width: 45vw
 </style>
