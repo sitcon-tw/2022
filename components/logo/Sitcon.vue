e9f8ebeceab29962f11e479ad28446f6daa9b075
gnehs <jayuiop@gmail.com>
2022-04-24 14:47:01 +0800

    updated /cfp

--- components/logo/Sitcon.vue
+++ components/logo/Sitcon.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="sitcon-title">學生計算機年會</div>
+  <h3 class="sitcon-title">學生計算機年會</h3>
 </template>
 <style lang="sass" scoped>
+
 .sitcon-title
-  font-size: 24px
   text-align: right
+  color: #32E0C4
+  line-height: 1
 </style>
--- pages/cfp/index.vue
+++ pages/cfp/index.vue
@@ -5,10 +5,34 @@
       <Title>{{ title }}</Title>
       <Meta name="description" :content="title" />
     </Head>
-    aaaa
+    <div class="container">
+      <logo-sitcon />
+
+      <logo-cat-in-a-maze />
+
+      <router-link to="/cfp/normal">一般議程</router-link>
+      <br>
+      <router-link to="/cfp/freeform">開放式議程</router-link>
+
+    </div>
     <Footer />
   </div>
 </template>
 <script setup>
 const title = ref('SITCON 2022 - Call for Papers')
 </script>
+<style lang="sass" scoped>
+
+.container
+  max-width: 960px
+  width: calc(100% - 64px)
+  padding: 40px 64px
+  margin: 0 auto
+  ::v-deep(.main-title)
+    justify-content: flex-end
+    gap: 96px
+    margin-top: -.5em
+    @media screen and (max-width: 768px)
+      font-size: 20px
+      gap: 32px
+</style>
