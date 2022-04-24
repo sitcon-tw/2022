166502ac24cc10d29449aa7fad6376cf705de669
Leko <leko@leko.moe>
2022-04-24 20:08:51 +0800

    main page first block

---
+++ components/cfp/PrevSite.vue
@@ -0,0 +1,28 @@
+<template>
+  <figure>
+    <img :src="src">
+    <figcaption>
+        <a :href="href">{{content}}</a>
+    </figcaption>
+</figure>
+</template>
+
+<script>
+export default {
+  props: ['src', 'content', 'href']
+}
+</script>
+<style lang="sass" scoped>
+figure 
+  margin: 0 auto
+  display: inline-block
+  text-align: center
+  img
+    vertical-align: top
+    width: 100%
+  figcaption
+    width: 100%
+  a
+    color: #53A99B
+    text-decoration: none
+</style>
--- pages/cfp/index.vue
+++ pages/cfp/index.vue
@@ -14,9 +14,11 @@
         <img src="/imgs/cfp/稿件徵求中.svg">
       </div>
 
-      <cfp-round-btn href="/cfp/normal" content="一般議程" />
-      <br>
-      <cfp-round-btn href="/cfp/freeform" content="開放式議程" />
+      <div class="round-btns">
+        <cfp-round-btn href="/cfp/normal" content="一般議程" />
+        <cfp-round-btn href="/cfp/freeform" content="開放式議程" />
+      </div>
+
       <br>
 
       <cfp-glass-box first-child>
@@ -24,6 +26,19 @@
         <p>SITCON 學生計算機年會自 2013 年發起，以學生為本、由學生自發舉辦，長期投身學生資訊教育與推廣開源精神，希望引領更多學子踏入資訊的殿堂，更冀望所有對資訊有興趣的學生，能夠在年會裏齊聚一堂，彼此激盪、傳承、啟發，達到「學以致用、教學相長」的實際展現。</p>
         <p>在 SITCON 的第 X 週年，期望我們秉持初衷——讓學生們分享與交流知識，把各地學生串連起來的 X 字路口——也期望每人皆能在與大家的聊天與討論之中，找到自己的方向、熱情。</p>
         <h3>SITCON 歷年網站</h3>
+        <div class="prev-sites">
+          <cfp-prev-site src="/imgs/cfp/prev-sites/2021.png" content="SITCON 2021" href="#" />
+          <cfp-prev-site src="/imgs/cfp/prev-sites/2020.png" content="SITCON 2020" href="#" />
+          <cfp-prev-site src="/imgs/cfp/prev-sites/2019.png" content="SITCON 2019" href="#" />
+          <cfp-prev-site src="/imgs/cfp/prev-sites/2018.png" content="SITCON 2018" href="#" />
+        </div>
+        <h3>參與討論</h3>
+        <p>SITCON 是個開放的社群，許多籌備的相關事項都會在公開的郵件論壇上進行討論，如果有興趣參與，歡迎訂閱 SITCON 的郵件論壇並貢獻你的想法！</p>
+        <p>若你想進一步了解過往討論的內容，也歡迎在 HackMD 上查看 SITCON 2022 的會議記錄。</p>
+        <div class="rad-btns">
+          <cfp-rad-btn href="#" content="SITCON 郵件論壇" />
+          <cfp-rad-btn href="#" content="籌備會議紀錄" />
+        </div>
       </cfp-glass-box>
 
     </div>
@@ -36,7 +51,6 @@ const title = ref('SITCON 2022 - Call for Papers')
 <style lang="sass" scoped>
 
 .container
-  max-width: 960px
   width: calc(100% - 64px)
   padding: 40px 64px
   margin: 0 auto
@@ -72,4 +86,24 @@ const title = ref('SITCON 2022 - Call for Papers')
       width: 45%
     @media screen and (max-width: 650px)
       width: 45vw
+
+.round-btns
+  display: grid
+  grid-row: 1
+  direction: rtl
+  grid-row-gap: 20px
+  @media screen and (max-width: 768px)
+    grid-row-gap: 15px
+
+.prev-sites
+  display: grid
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr))
+
+.rad-btns
+  display: inline-flex
+  *
+    margin-right: 64px
+  @media screen and (max-width: 768px)
+    margin-right: 45px
+
 </style>
