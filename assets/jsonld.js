011b2057f3a071072aa1e89aaece53a270e9f4df
Denny Huang <denny0223@gmail.com>
2022-04-24 23:43:29 +0800

    updated json-ld

--- assets/jsonld.js
+++ assets/jsonld.js
@@ -1,5 +1,109 @@
 export default {
-  '@context': 'https://schema.org',
-  '@type': 'Thing',
-  name: 'static json',
+  "@context": "https://schema.org",
+  "@type": "Event",
+  "name": "SITCON 學生計算機年會 2022",
+  "url": "https://sitcon.org/2022/",
+  "startDate": "2022-09-04T08:30+08:00",
+  "eventAttendanceMode": "https://schema.org/OfflineEventAttendanceMode",
+  "location": {
+    "@type": "EventVenue",
+    "name": "中央研究院人文社會科學館 國際會議廳 ",
+    "address": "台北市南港區研究院路二段128號"
+  },
+  "organizer": {
+    "@type": "Organization",
+    "url": "https://sitcon.org",
+    "name": "SITCON 學生計算機年會",
+    "logo": "https://sitcon.org/2022/imgs/logo.png",
+    "sameAs": [
+      "https://www.youtube.com/user/SITCONArchive",
+      "https://www.facebook.com/SITCONtw/",
+      "https://twitter.com/sitcontw"
+    ]
+  },
+  "image": [
+    "https://sitcon.org/2022/imgs/cfp-OG-image.png"
+  ],
+  "description": "SITCON X - Cat in a Maze 學學貓咪，換個思維邏輯面對迷惘的現況，保持著好奇心與探索的樂趣，跳脫框架創造屬於自己的美好未來！",
+  "endDate": "2022-09-04T18:00+08:00",
+  "performer": {
+    "@type": "Organization",
+    "url": "https://sitcon.org",
+    "name": "SITCON 學生計算機年會",
+    "logo": "https://sitcon.org/2022/imgs/logo.png",
+    "sameAs": [
+      "https://www.youtube.com/user/SITCONArchive",
+      "https://www.facebook.com/SITCONtw/",
+      "https://twitter.com/sitcontw"
+    ]
+  },
+  "offers": [
+    {
+      "@type": "Offer",
+      "name": "小額贊助 10000 元",
+      "category": "individual-sponsor",
+      "price": 10000,
+      "priceCurrency": "TWD",
+      "availability": "InStock",
+      "url": "https://ocf.neticrm.tw/civicrm/contribute/transact?reset=1&id=55",
+      "validFrom": "2022-04-17T00:00:00.000+08:00",
+      "validThrough": "2022-09-04T17:00:00.000+08:00"
+    },
+    {
+      "@type": "Offer",
+      "name": "小額贊助 5000 元",
+      "category": "individual-sponsor",
+      "price": 5000,
+      "priceCurrency": "TWD",
+      "availability": "InStock",
+      "url": "https://ocf.neticrm.tw/civicrm/contribute/transact?reset=1&id=55",
+      "validFrom": "2022-04-17T00:00:00.000+08:00",
+      "validThrough": "2022-09-04T17:00:00.000+08:00"
+    },
+    {
+      "@type": "Offer",
+      "name": "小額贊助 3500 元",
+      "category": "individual-sponsor",
+      "price": 3500,
+      "priceCurrency": "TWD",
+      "availability": "InStock",
+      "url": "https://ocf.neticrm.tw/civicrm/contribute/transact?reset=1&id=55",
+      "validFrom": "2022-04-17T00:00:00.000+08:00",
+      "validThrough": "2022-09-04T17:00:00.000+08:00"
+    },
+    {
+      "@type": "Offer",
+      "name": "小額贊助 2500 元",
+      "category": "individual-sponsor",
+      "price": 2500,
+      "priceCurrency": "TWD",
+      "availability": "InStock",
+      "url": "https://ocf.neticrm.tw/civicrm/contribute/transact?reset=1&id=55",
+      "validFrom": "2022-04-17T00:00:00.000+08:00",
+      "validThrough": "2022-09-04T17:00:00.000+08:00"
+    },
+    {
+      "@type": "Offer",
+      "name": "小額贊助 1000 元",
+      "category": "individual-sponsor",
+      "price": 1000,
+      "priceCurrency": "TWD",
+      "availability": "InStock",
+      "url": "https://ocf.neticrm.tw/civicrm/contribute/transact?reset=1&id=55",
+      "validFrom": "2022-04-17T00:00:00.000+08:00",
+      "validThrough": "2022-09-04T17:00:00.000+08:00"
+    },
+    {
+      "@type": "Offer",
+      "name": "個人小額贊助方案",
+      "category": "individual-sponsor",
+      "price": 500,
+      "priceCurrency": "TWD",
+      "availability": "InStock",
+      "url": "https://ocf.neticrm.tw/civicrm/contribute/transact?reset=1&id=55",
+      "validFrom": "2022-04-17T00:00+08:00"
+      "validThrough": "2022-09-04T17:00:00.000+08:00"
+    }
+  ],
+  "eventStatus": "EventScheduled"
 }
