4dcb4331392ef1173802ecc0c367670da9239aad
gnehs <jayuiop@gmail.com>
2022-04-25 22:50:42 +0800

    fixed missing root class first-loading

--- pages/cfp/freeform.vue
+++ pages/cfp/freeform.vue
@@ -1,64 +1,65 @@
  <template>
-  <div class="maze-background">
+  <div>
 
     <Head>
       <Title>{{ title }}</Title>
       <Meta name="description" :content="title" />
     </Head>
-
-    <cfp-glass-box first-child>
-      <cfp-back-btn />
-      <logo-cat-in-a-maze />
-      <h1 class="text-center">開放式議程</h1>
-      <cfp-divider />
-      <cfp-timer />
-      <cfp-submission-btn href="https://pancake.gnehs.net/" />
-      <cfp-divider />
-      <cfp-block-title>重要時程</cfp-block-title>
-      <cfp-schedule />
-      <cfp-block-title>議程種類</cfp-block-title>
-      <cfp-agenda-type />
-      <cfp-block-title>什麼是開放式議程？</cfp-block-title>
-      <p>在追求創新的時代，我們為講者提供了開放式議程這個選項，跳脫講者對聽眾<strong>單向的演講（陳述）</strong>，開放式議程更加強調主講者和與會者的雙向交流。SITCON 作為學生展現自己的舞台，我們期待以學生為主體的投稿，可以是從學生角度出發的經驗分享、技術分享，或是一同探討學生相關議題等。
-        <br>
-        <br>
-        對於還在猶豫的講者們，<strong>以下是提供給各位參考的幾種議程形式範例：</strong>
-      </p>
-      <cfp-session-type-card />
-      <cfp-block-title>Code of Conduct 行為準則</cfp-block-title>
-      <p>SITCON 歡迎不同身分、來自不同背景的與會者，我們鼓勵女性、性少數與多元背景的參與者。為了讓大家都能愉快的參加 SITCON，我們要求所有參與者閱讀年會的<a href="https://sitcon.org/code-of-conduct/" target="_blank">
-          行為守則(Code of Conduct) </a>，共同創造一個友善的環境。 </p>
-    </cfp-glass-box>
-    <cfp-glass-box>
-      <cfp-block-title>大河馬</cfp-block-title>
-      <p>segksngb;bs gegege geg eg eg eg </p>
-      <ul>
-        <li>
-          <strong>大河馬：</strong>蓬蓬鬆餅
-        </li>
-        <li>哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？</li>
-      </ul>
-    </cfp-glass-box>
-    <cfp-glass-box last-child>
-      <cfp-block-title>Q&A</cfp-block-title>
-      <cfp-question-box>
-        <template v-slot:title>哪裡可以看到大河馬？</template>
-        動物園都可以看到大河馬
-      </cfp-question-box>
-      <cfp-question-box>
-        <template v-slot:title>哪裡可以看到大河馬？</template>
-        動物園都可以看到大河馬
-      </cfp-question-box>
-      <cfp-question-box>
-        <template v-slot:title>哪裡可以看到大河馬？</template>
-        動物園都可以看到大河馬
-      </cfp-question-box>
-      <cfp-question-box>
-        <template v-slot:title>哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？</template>
-        動物園都可以看到大河馬
-      </cfp-question-box>
-    </cfp-glass-box>
-    <Footer />
+    <div class="maze-background">
+      <cfp-glass-box first-child>
+        <cfp-back-btn />
+        <logo-cat-in-a-maze />
+        <h1 class="text-center">開放式議程</h1>
+        <cfp-divider />
+        <cfp-timer />
+        <cfp-submission-btn href="https://pancake.gnehs.net/" />
+        <cfp-divider />
+        <cfp-block-title>重要時程</cfp-block-title>
+        <cfp-schedule />
+        <cfp-block-title>議程種類</cfp-block-title>
+        <cfp-agenda-type />
+        <cfp-block-title>什麼是開放式議程？</cfp-block-title>
+        <p>在追求創新的時代，我們為講者提供了開放式議程這個選項，跳脫講者對聽眾<strong>單向的演講（陳述）</strong>，開放式議程更加強調主講者和與會者的雙向交流。SITCON 作為學生展現自己的舞台，我們期待以學生為主體的投稿，可以是從學生角度出發的經驗分享、技術分享，或是一同探討學生相關議題等。
+          <br>
+          <br>
+          對於還在猶豫的講者們，<strong>以下是提供給各位參考的幾種議程形式範例：</strong>
+        </p>
+        <cfp-session-type-card />
+        <cfp-block-title>Code of Conduct 行為準則</cfp-block-title>
+        <p>SITCON 歡迎不同身分、來自不同背景的與會者，我們鼓勵女性、性少數與多元背景的參與者。為了讓大家都能愉快的參加 SITCON，我們要求所有參與者閱讀年會的<a href="https://sitcon.org/code-of-conduct/" target="_blank">
+            行為守則(Code of Conduct) </a>，共同創造一個友善的環境。 </p>
+      </cfp-glass-box>
+      <cfp-glass-box>
+        <cfp-block-title>大河馬</cfp-block-title>
+        <p>segksngb;bs gegege geg eg eg eg </p>
+        <ul>
+          <li>
+            <strong>大河馬：</strong>蓬蓬鬆餅
+          </li>
+          <li>哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？</li>
+        </ul>
+      </cfp-glass-box>
+      <cfp-glass-box last-child>
+        <cfp-block-title>Q&A</cfp-block-title>
+        <cfp-question-box>
+          <template v-slot:title>哪裡可以看到大河馬？</template>
+          動物園都可以看到大河馬
+        </cfp-question-box>
+        <cfp-question-box>
+          <template v-slot:title>哪裡可以看到大河馬？</template>
+          動物園都可以看到大河馬
+        </cfp-question-box>
+        <cfp-question-box>
+          <template v-slot:title>哪裡可以看到大河馬？</template>
+          動物園都可以看到大河馬
+        </cfp-question-box>
+        <cfp-question-box>
+          <template v-slot:title>哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？哪裡可以看到大河馬？</template>
+          動物園都可以看到大河馬
+        </cfp-question-box>
+      </cfp-glass-box>
+      <Footer />
+    </div>
   </div>
 </template>
 <script setup>
--- pages/cfp/index.vue
+++ pages/cfp/index.vue
@@ -1,59 +1,63 @@
 <template>
-  <div class="cfp-main-page">
+  <div>
 
     <Head>
       <Title>{{ title }}</Title>
       <Meta name="description" :content="title" />
     </Head>
-    <div class="container">
-      <div class="logos">
-        <logo-sitcon />
-        <logo-cat-in-a-maze />
-      </div>
-      <div class="稿件徵求中">
-        <img src="/2022/imgs/cfp/稿件徵求中.svg">
-      </div>
+    <div class="cfp-main-page">
 
-      <cfp-date-links />
 
-      <br>
+      <div class="container">
+        <div class="logos">
+          <logo-sitcon />
+          <logo-cat-in-a-maze />
+        </div>
+        <div class="稿件徵求中">
+          <img src="/2022/imgs/cfp/稿件徵求中.svg">
+        </div>
+
+        <cfp-date-links />
+
+        <br>
+      </div>
+      <cfp-rounded-glass-box>
+        <h3>關於 SITCON</h3>
+        <p>SITCON 學生計算機年會自 2013 年發起，以學生為本、由學生自發舉辦，長期投身學生資訊教育與推廣開源精神，希望引領更多學子踏入資訊的殿堂，更冀望所有對資訊有興趣的學生，能夠在年會裏齊聚一堂，彼此激盪、傳承、啟發，達到「學以致用、教學相長」的實際展現。</p>
+        <p>在 SITCON 的第 X 週年，期望我們秉持初衷——讓學生們分享與交流知識，把各地學生串連起來的 X 字路口——也期望每人皆能在與大家的聊天與討論之中，找到自己的方向、熱情。</p>
+        <h3>SITCON 歷年網站</h3>
+        <div class="prev-sites">
+          <cfp-prev-site src="/2022/imgs/cfp/prev-sites/2021.png" href="https://sitcon.org/2021/">SITCON 2021</cfp-prev-site>
+          <cfp-prev-site src="/2022/imgs/cfp/prev-sites/2020.png" href="https://sitcon.org/2020/">SITCON 2020</cfp-prev-site>
+          <cfp-prev-site src="/2022/imgs/cfp/prev-sites/2019.png" href="https://sitcon.org/2019/">SITCON 2019</cfp-prev-site>
+          <cfp-prev-site src="/2022/imgs/cfp/prev-sites/2018.png" href="https://sitcon.org/2018/">SITCON 2018</cfp-prev-site>
+        </div>
+        <h3>參與討論</h3>
+        <p>SITCON 是個開放的社群，許多籌備的相關事項都會在公開的郵件論壇上進行討論，如果有興趣參與，歡迎訂閱 SITCON 的郵件論壇並貢獻你的想法！</p>
+        <p>若你想進一步了解過往討論的內容，也歡迎在 HackMD 上查看 SITCON 2022 的會議記錄。</p>
+        <div class="rad-btns">
+          <cfp-rad-btn href="#" content="SITCON 郵件論壇" />
+          <cfp-rad-btn href="#" content="籌備會議紀錄" />
+        </div>
+      </cfp-rounded-glass-box>
+
+      <cfp-rounded-glass-box class="cat-background">
+        <h3 class="margin-0">2022 年會主題</h3>
+        <h1 class="margin-0">Cat in a Maze</h1>
+        <p>Cat in a Maze 參考自 Rat in a Maze 這道經典演算法題目。題目描述一隻老鼠要走迷宮，在給定了迷宮形狀的前提下，要努力找出走出迷宮的出路。「Rat in a Maze」這句話切實地反映了劇烈動盪時代下的學生們在面對未來的生涯發展時，猶如一隻無法逃避、勢必得進入體制這一迷宮的老鼠。</p>
+        <h3 class="text-center">但是⋯⋯我們真的只能當老鼠嗎？</h3>
+        <p>不如，像貓咪一樣思考？以貓咪的個性，第一直覺想到的會是遇到迷宮時，直接破壞迷宮離開，或者是使用我們想不到的方式解決迷宮。同時貓咪相較於老鼠，對一切事物充滿好奇、敢於探索未知事物，不畏強權挑戰一切，是貓咪獨特的個性。<br>
+          對於學生而言，充滿好奇、敢於探索未知事物相當重要，畢竟自己的未來要由自己來定義。當大家陷入迷惘的時候，比起使用老鼠告訴大家已經陷入迷惘了，不如讓我們可以看看貓咪、學學貓咪，換個思維邏輯去面對迷惘的現況，保持著好奇心與探索的樂趣。又或者跳脫框架，創造屬於自己的美好未來。</p>
+        <h3>SITCON 贊助徵求</h3>
+        <p>SITCON 的舉辦，除了有賴眾多志工的努力，也需要金錢的支持。<br>如果您認同 SITCON 的理念，歡迎以行動支持 SITCON 的運作，讓更多交流產生！</p>
+        <p>企業合作歡迎聯繫 <a href="mailto:contact@sitcon.org">contact@sitcon.org</a></p>
+        <div class="rad-btns">
+          <cfp-rad-btn href="#" content="個人贊助方案" />
+        </div>
+      </cfp-rounded-glass-box>
+
+      <Footer />
     </div>
-    <cfp-rounded-glass-box>
-      <h3>關於 SITCON</h3>
-      <p>SITCON 學生計算機年會自 2013 年發起，以學生為本、由學生自發舉辦，長期投身學生資訊教育與推廣開源精神，希望引領更多學子踏入資訊的殿堂，更冀望所有對資訊有興趣的學生，能夠在年會裏齊聚一堂，彼此激盪、傳承、啟發，達到「學以致用、教學相長」的實際展現。</p>
-      <p>在 SITCON 的第 X 週年，期望我們秉持初衷——讓學生們分享與交流知識，把各地學生串連起來的 X 字路口——也期望每人皆能在與大家的聊天與討論之中，找到自己的方向、熱情。</p>
-      <h3>SITCON 歷年網站</h3>
-      <div class="prev-sites">
-        <cfp-prev-site src="/2022/imgs/cfp/prev-sites/2021.png" href="https://sitcon.org/2021/">SITCON 2021</cfp-prev-site>
-        <cfp-prev-site src="/2022/imgs/cfp/prev-sites/2020.png" href="https://sitcon.org/2020/">SITCON 2020</cfp-prev-site>
-        <cfp-prev-site src="/2022/imgs/cfp/prev-sites/2019.png" href="https://sitcon.org/2019/">SITCON 2019</cfp-prev-site>
-        <cfp-prev-site src="/2022/imgs/cfp/prev-sites/2018.png" href="https://sitcon.org/2018/">SITCON 2018</cfp-prev-site>
-      </div>
-      <h3>參與討論</h3>
-      <p>SITCON 是個開放的社群，許多籌備的相關事項都會在公開的郵件論壇上進行討論，如果有興趣參與，歡迎訂閱 SITCON 的郵件論壇並貢獻你的想法！</p>
-      <p>若你想進一步了解過往討論的內容，也歡迎在 HackMD 上查看 SITCON 2022 的會議記錄。</p>
-      <div class="rad-btns">
-        <cfp-rad-btn href="#" content="SITCON 郵件論壇" />
-        <cfp-rad-btn href="#" content="籌備會議紀錄" />
-      </div>
-    </cfp-rounded-glass-box>
-
-    <cfp-rounded-glass-box class="cat-background">
-      <h3 class="margin-0">2022 年會主題</h3>
-      <h1 class="margin-0">Cat in a Maze</h1>
-      <p>Cat in a Maze 參考自 Rat in a Maze 這道經典演算法題目。題目描述一隻老鼠要走迷宮，在給定了迷宮形狀的前提下，要努力找出走出迷宮的出路。「Rat in a Maze」這句話切實地反映了劇烈動盪時代下的學生們在面對未來的生涯發展時，猶如一隻無法逃避、勢必得進入體制這一迷宮的老鼠。</p>
-      <h3 class="text-center">但是⋯⋯我們真的只能當老鼠嗎？</h3>
-      <p>不如，像貓咪一樣思考？以貓咪的個性，第一直覺想到的會是遇到迷宮時，直接破壞迷宮離開，或者是使用我們想不到的方式解決迷宮。同時貓咪相較於老鼠，對一切事物充滿好奇、敢於探索未知事物，不畏強權挑戰一切，是貓咪獨特的個性。<br>
-        對於學生而言，充滿好奇、敢於探索未知事物相當重要，畢竟自己的未來要由自己來定義。當大家陷入迷惘的時候，比起使用老鼠告訴大家已經陷入迷惘了，不如讓我們可以看看貓咪、學學貓咪，換個思維邏輯去面對迷惘的現況，保持著好奇心與探索的樂趣。又或者跳脫框架，創造屬於自己的美好未來。</p>
-      <h3>SITCON 贊助徵求</h3>
-      <p>SITCON 的舉辦，除了有賴眾多志工的努力，也需要金錢的支持。<br>如果您認同 SITCON 的理念，歡迎以行動支持 SITCON 的運作，讓更多交流產生！</p>
-      <p>企業合作歡迎聯繫 <a href="mailto:contact@sitcon.org">contact@sitcon.org</a></p>
-      <div class="rad-btns">
-        <cfp-rad-btn href="#" content="個人贊助方案" />
-      </div>
-    </cfp-rounded-glass-box>
-
-    <Footer />
   </div>
 </template>
 <script setup>
