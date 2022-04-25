69fdc5aa201435ec33ff7b2743868d8161a5c101
gnehs <jayuiop@gmail.com>
2022-04-25 14:59:09 +0800

    updated baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   app: {
+    baseURL: './',
     head: {
       titleTemplate: '%s  | SITCON 2022 學生計算機年會 | Students\' Information Technology Conference',
       link: [
