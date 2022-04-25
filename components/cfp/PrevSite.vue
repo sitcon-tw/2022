816451c8dd997ed22f1ca3fac0d74676170b1874
gnehs <jayuiop@gmail.com>
2022-04-25 12:08:47 +0800

    added RoundedGlassBox, fixed PrevSite, fixed cat

--- components/cfp/PrevSite.vue
+++ components/cfp/PrevSite.vue
@@ -1,28 +1,48 @@
 <template>
-  <figure>
-    <img :src="src">
-    <figcaption>
-        <a :href="href">{{content}}</a>
-    </figcaption>
-</figure>
+  <a :href="href" target="_blank">
+    <figure>
+      <img :src="src">
+      <figcaption>
+        <slot />
+      </figcaption>
+    </figure>
+  </a>
 </template>
 
 <script>
 export default {
-  props: ['src', 'content', 'href']
+  props: ['src', 'href']
 }
 </script>
 <style lang="sass" scoped>
-figure 
-  margin: 0 auto
-  display: inline-block
-  text-align: center
-  img
-    vertical-align: top
-    width: 100%
-  figcaption
-    width: 100%
-  a
-    color: #53A99B
-    text-decoration: none
+
+a
+  text-decoration: none
+
+  figure
+    margin: 0 auto
+    display: inline-block
+    text-align: center
+    img
+      vertical-align: top
+      width: 100%
+      aspect-ratio: 16/9
+      object-fit: cover
+      object-position: top center
+      border-radius: 16px
+      transition: all 0.3s ease
+      @media screen and (max-width: 768px)
+        border-radius: 8px
+    figcaption
+      width: 100%
+      color: #53A99B
+      transition: all 0.3s ease
+      @media screen and (max-width: 768px)
+        font-weight: 300
+  &:hover
+    img
+      transform: scale(1.01) translateY(-2.5px)
+      box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.85)
+    figcaption
+      color: #fff
 </style>
---
+++ components/cfp/RoundedGlassBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <cfp-glass-box class="rounded">
+    <slot />
+  </cfp-glass-box>
+</template>
+ 
+<style lang="sass" scoped>
+.rounded.rounded
+  border-radius: 56px
+  padding: 72px
+  @media screen and (max-width: 768px)
+    border-radius: 20px
+    padding: 20px
+</style>
