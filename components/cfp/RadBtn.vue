c26b4e442cd24c4aea75226e18f31c0faabc5ccf
gnehs <jayuiop@gmail.com>
2022-04-26 16:17:41 +0800

    updated rad-btn

--- components/cfp/RadBtn.vue
+++ components/cfp/RadBtn.vue
@@ -1,8 +1,6 @@
 <template>
-  <a :href="href" target="_blank">
-    <span>
-      {{ content }}
-    </span>
+  <a :href="href" class="rad-btn" target="_blank">
+    {{ content }}
   </a>
 </template>
 
@@ -12,25 +10,20 @@ export default {
 }
 </script>
 <style lang="sass" scoped>
-  a
-    margin: 0 auto
-    padding: 0 20px
-    display: block
-    width: max-content
-    white-space: nowrap
-    text-align: center
-    font-size: 24px
-    color: #fff
-    background-color: #32E0C4
-    border: 1px solid #32E0C4
-    text-decoration: none
-    font-weight: 700
-    border-radius: 10px
-    span
-      letter-spacing: 0.1em
-    &:hover
-      background-color: transparent
-    @media screen and (max-width: 768px)
-        font-size: 18px
-        padding: 0 15px
+.rad-btn
+  padding: 0 1em
+  display: block
+  text-align: center
+  font-size: 24px
+  color: #fff
+  background-color: #32E0C4
+  border: 1px solid #32E0C4
+  text-decoration: none
+  font-weight: 700
+  border-radius: .5em
+  transition: all 0.3s ease
+  @media screen and (max-width: 768px)
+    font-size: 18px
+  &:hover
+    background-color: transparent
 </style>
