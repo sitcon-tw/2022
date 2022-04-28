6e727bd85d539b0615786f06922b75361e0ef62f
gnehs <jayuiop@gmail.com>
2022-04-28 21:05:49 +0800

    updated gtag

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -3,9 +3,10 @@ import VueGtag from 'vue-gtag'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     config: {
-      id: "UA-34467841-1",
+      id: "G-GVRT4CHSQJ",
     },
     includes: [
+      { id: 'UA-34467841-1' },
       { id: 'GTM-NPVBCDZ' }
     ]
   }, nuxtApp.vueApp.router)
