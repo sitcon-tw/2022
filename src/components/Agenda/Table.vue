6c125832f7734046df39dc0f9fa8674682c6943f
gnehs <jayuiop@gmail.com>
2022-05-27 18:26:27 +0800

    removed parseTime second

--- src/components/Agenda/Table.vue
+++ src/components/Agenda/Table.vue
@@ -111,7 +111,6 @@ export default {
       return new Date(time).toLocaleTimeString('en-US', {
         hour: '2-digit',
         minute: '2-digit',
-        second: '2-digit',
         hour12: false,
       }).replace(/\:/g, '')
     },
