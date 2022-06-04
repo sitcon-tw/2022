cb649c84c88aa4f2f3e84a04c0837cc5939535c1
gnehs <jayuiop@gmail.com>
2022-06-04 12:11:10 +0800

    updated footer sponsor

--- src/components/FooterItem.vue
+++ src/components/FooterItem.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <mint-box v-if="!hideSponsor">
-      :pancake:
+    <mint-box v-if="!hideSponsor" class="sopnsor-items">
+      <img class="sponsor" :src="'/2022/imgs/pancake/sponsor.png'" />
+      <img class="sponsor" :src="'/2022/imgs/pancake/sponsor.png'" />
+      <img class="sponsor" :src="'/2022/imgs/pancake/sponsor.png'" />
+      <img class="sponsor" :src="'/2022/imgs/pancake/sponsor.png'" />
+      <img class="sponsor" :src="'/2022/imgs/pancake/sponsor.png'" />
+      <img class="sponsor" :src="'/2022/imgs/pancake/sponsor.png'" />
+      <img class="sponsor" :src="'/2022/imgs/pancake/sponsor.png'" />
+      <img class="sponsor" :src="'/2022/imgs/pancake/sponsor.png'" />
     </mint-box>
     <div>
       sitcon
@@ -17,3 +24,11 @@ const props = defineProps({
   }
 })
 </script>
+<style lang="sass" scoped>
+.sopnsor-items
+  display: grid
+  grid-template-columns: repeat(4, 1fr)
+  grid-gap: 20px
+  img
+    width: 100%
+</style>
