a6a2cbdc1725cb604fff972301487d6c5c21e932
gnehs <jayuiop@gmail.com>
2022-06-05 01:19:59 +0800

    updated staff page

--- src/pages/staff.vue
+++ src/pages/staff.vue
@@ -1,7 +1,42 @@
 <template>
   <div class="container">
     <nav-bar />
-    staff
+    <div class="box" v-for="item of teams" :key="item.name">
+      <h1>{{ item.name }}</h1>
+      <p>{{ item.intro }}</p>
+      <div class="member-items">
+        <div class="member-item" v-for="item of item.members">
+          <img :src="`https://www.gravatar.com/avatar/${item.emailHash}?s=320&d=${origin}/2022/imgs/deafult_avatar.jpg&r=g`" />
+          <div class="member-name">{{ item.name }}</div>
+          <div class="member-intro">{{ item.type }}</div>
+        </div>
+      </div>
+    </div>
     <footer-item />
   </div>
 </template>
+<script>
+import teams from '@/assets/teams.json'
+export default {
+  data() {
+    return {
+      teams,
+      origin: location.origin
+    }
+  }
+}
+</script>
+<style lang="sass" scoped>
+.member-items
+  display: grid
+  grid-template-columns: repeat(auto-fit, 80px)
+  grid-gap: 20px
+  margin-top: 20px
+  .member-item
+    display: flex
+    flex-direction: column
+    align-items: center
+    justify-content: center
+    img
+      width: 100%
+</style>
