3e77aa2d7edd9525b419f4713aad206f24f77624
gnehs <jayuiop@gmail.com>
2022-06-06 15:33:05 +0800

    added cat

--- src/pages/traffic.vue
+++ src/pages/traffic.vue
@@ -4,7 +4,7 @@
 
     <arrow-box>
       <block-title>交通方式</block-title>
-      <mint-box>
+      <mint-box class="with-cat">
         <div class="location-title">地點</div>
         <p class="location-description">中央研究院人文社會科學館<br />
           台北市南港區研究院路二段 128 號</p>
@@ -13,12 +13,26 @@
         </client-only>
         <img class="traffic-flow" :src="'/2022/imgs/traffic/traffic-flow.svg'" />
         <img class="traffic-flow-mobile" :src="'/2022/imgs/traffic/traffic-flow-mobile.svg'" />
+        <img class="the-white-neko" :src="'/2022/imgs/cats/cat3.svg'" />
       </mint-box>
     </arrow-box>
     <footer-item />
   </div>
 </template>
 <style lang="sass" scoped>
+.with-cat
+  position: relative
+  .the-white-neko
+    position: absolute
+    --size: 150px
+    top:  calc(var(--size) * -0.6)
+    height: calc(var(--size) * 0.6)
+    right: 5vw
+    width: var(--size)
+    object-fit: cover
+    object-position: top right
+    @media (max-width: 768px)
+      --size: 100px
 .location-title
   font-size: 32px
   font-weight: bold
