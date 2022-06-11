ce809f4a99a61efadc06f7cce0336cfe3f70b823
gnehs <jayuiop@gmail.com>
2022-06-11 20:10:17 +0800

    added hide-sponsor

--- src/layout/default.vue
+++ src/layout/default.vue
@@ -3,6 +3,15 @@
   <router-view />
   <div class="container">
 
-    <footer-item />
+    <footer-item :hide-sponsor="hideSponsor" />
   </div>
 </template>
+<script>
+export default {
+  computed: {
+    hideSponsor() {
+      return this.$route.path.indexOf('/sponsor') === 0
+    }
+  },
+}
+</script>
