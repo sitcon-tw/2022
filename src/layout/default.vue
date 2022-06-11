b6cdc7430dc90327d260ea6fdbbb8ab48b3ad8ef
gnehs <jayuiop@gmail.com>
2022-06-11 20:16:30 +0800

    added loading

--- src/layout/default.vue
+++ src/layout/default.vue
@@ -1,8 +1,8 @@
 <template>
   <nav-bar />
-  <router-view />
+  <router-view v-if="!showLoading" />
+  <div class="loading" v-if="showLoading">loading...</div>
   <div class="container">
-
     <footer-item :hide-sponsor="hideSponsor" />
   </div>
 </template>
@@ -13,5 +13,20 @@ export default {
       return this.$route.path.indexOf('/sponsor') === 0
     }
   },
+  data() {
+    return {
+      showLoading: true,
+    }
+  },
+  mounted() {
+    this.$router.beforeEach((to, from, next) => {
+      this.showLoading = true
+      next()
+    })
+    this.$router.afterEach(() => {
+      this.showLoading = false
+    })
+    this.showLoading = false
+  }
 }
 </script>
