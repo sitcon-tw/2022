99e3f0f71640464e63bff74f094a70e48510f12e
Allen Guan <me@nella17.tw>
2022-06-19 06:30:09 +0800

    remove inline svg size

--- src/components/svg/Arrow.vue
+++ src/components/svg/Arrow.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg width="100%" height="100%" viewBox="0 0 107 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 107 53" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path class="switch" fill-rule="evenodd" clip-rule="evenodd" d="M0.296875 0L22.4119 26.5L0.296875 53H20.3134L42.5898 26.5L20.3134 0H0.296875Z"/>
         <path class="switch" fill-rule="evenodd" clip-rule="evenodd" d="M42.59 0L64.7536 26.5L42.59 53H84.6688L106.832 26.5L84.6688 0H42.59Z"/>
     </svg>
