0a9367236ccde84c75775debd2835dbbb3868826
gnehs <jayuiop@gmail.com>
2022-06-24 23:59:57 +0800

    updated description

--- src/pages/sponsor.vue
+++ src/pages/sponsor.vue
@@ -3,7 +3,7 @@
     <GeneralHead
       page-title="贊助"
       title="贊助 | SITCON X"
-      description="在 SITCON 的第 X 週年，期望我們秉持初衷——讓學生們分享與交流知識，把各地學生串連起來的 X 字路口——也期望每個人能夠向貓咪學習，換個思維邏輯面對迷惘的現況，保持著好奇心與探索的樂趣，跳脫框架創造屬於自己的美好未來！"
+      description="認同學生計算機年會的理念，歡迎透過個人小額贊助支持，或與我們聯繫取得更多企業合作資訊。"
       image="https://sitcon.org/2022/imgs/og.jpg" />
 
     <arrow-box>
