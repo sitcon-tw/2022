9b0155411f6b46096888c48ef8d85477515c4103
gnehs <jayuiop@gmail.com>
2022-08-28 21:33:31 +0800

    updated vite-ssg config

--- vite.config.js
+++ vite.config.js
@@ -19,5 +19,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  ssgOptions: { formatting: "minify" },
+  ssgOptions: {
+    dirStyle: 'nested',
+    formatting: "minify"
+  },
 })
