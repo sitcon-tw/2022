43696c9eb99dd558fddad336be11393807cd5baa
denny0223 <denny0223@users.noreply.github.com>
2024-11-18 17:35:06 +0800

    deploy: 0f00fdae5905afa275d519e8849b642a8ce68db5

---
+++ assets/BlockTitle.4a5a6c89.js
@@ -0,0 +1,1 @@
+import{_,o as n,a as r,r as t}from"./app.aaa4f4e9.js";const o={};function a(e,s){return n(),r("h1",null,[t(e.$slots,"default",{},void 0,!0)])}var l=_(o,[["render",a],["__scopeId","data-v-0b75a4e2"]]);export{l as _};
